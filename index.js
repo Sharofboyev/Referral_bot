30c6875f518efec9a8d93f47012698a6287bcc0c
Sarvar <sharofboyev.sarvar@gmail.com>
2022-12-15 18:34:26 +0500

    Added feature getting referral link

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const userService = new User();
 const referralService = new Referral()
 const path = require("path")
 const TelegrafI18n = require('telegraf-i18n')
+const {match} = require("telegraf-i18n")
 const i18n = new TelegrafI18n({
     defaultLanguage: 'ru',
     directory: path.resolve(__dirname, 'locales')
@@ -20,18 +21,26 @@ bot.use(async (ctx, next) => {
 })
 
 bot.start(async (ctx) => {
-    if (ctx.state)
-        return ctx.reply(ctx.i18n.t("greeting"));
-    
-    await userService.create(ctx.from.id, ctx.from.username, 
-        ctx.from.first_name + (ctx.from.last_name ? " " + ctx.from.last_name: ""));
-    let referrer = ctx.message.text.substring(7);
-    if (referrer.length > 0 && referrer.startsWith("reff")){
-        referrer = referrer.substring(4);
+    if (!ctx.state){
+        await userService.create(ctx.from.id, ctx.from.username, 
+            ctx.from.first_name + (ctx.from.last_name ? " " + ctx.from.last_name: ""));
+        let referrer = ctx.message.text.substring(7);
+        if (referrer.length > 0 && referrer.startsWith("reff")){
+            referrer = referrer.substring(4);
+        }
+        referrer = Number(referrer);
+        await referralService.addUser(ctx.from.id, referrer);
     }
-    referrer = Number(referrer);
-    await referralService.addUser(ctx.from.id, referrer);
-    return ctx.reply(ctx.i18n.t("greeting"))
+    return ctx.reply(ctx.i18n.t("greeting"), {reply_markup: {keyboard: 
+        [
+            [{text: ctx.i18n.t("get_link")}]
+        ],
+        resize_keyboard: true}});
+})
+
+bot.hears(match("get_link"), (ctx) => {
+    const link = referralService.getReferralLink(ctx.from.id)
+    return ctx.reply(ctx.i18n.t("your_link") + link);
 })
 
 bot.catch((err) => {
